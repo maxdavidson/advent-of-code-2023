d69795adb54df2dd3682083f4c4c82d91e0675c7
Max Davidson <max.davidsson@wbd.com>
2023-11-14 22:41:19 +0100

    Initial commit

---
+++ src/day00/day00.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} input
+ */
+export function part1(input) {
+  return input.trim().length;
+}
+
+/**
+ * @param {string} input
+ */
+export function part2(input) {
+  return input.trim().length;
+}
