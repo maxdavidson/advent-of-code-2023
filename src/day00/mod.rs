4f1dd54158f3fb02c375518951d8a78b208180bf
Max Davidson <max.davidsson@wbd.com>
2023-11-30 15:32:36 +0100

    Add sample problem

---
+++ src/day00/mod.rs
@@ -0,0 +1,27 @@
+pub fn part1(input: &str) -> u32 {
+  0
+}
+
+pub fn part2(input: &str) -> u32 {
+  0
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  const INPUT_TEST: &str = include_str!("input_test.txt");
+  const INPUT: &str = include_str!("input.txt");
+
+  #[test]
+  fn part1_works() {
+    assert_eq!(part1(INPUT_TEST), 0);
+    assert_eq!(part1(INPUT), 0);
+  }
+
+  #[test]
+  fn part2_works() {
+    assert_eq!(part2(INPUT_TEST), 0);
+    assert_eq!(part2(INPUT), 0);
+  }
+}
