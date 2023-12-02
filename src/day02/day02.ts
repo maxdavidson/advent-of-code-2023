ac4efc7d16064adb1096b176506398546ededec5
Max Davidson <max.davidsson@wbd.com>
2023-12-02 16:04:55 +0100

    Simplify day 02 even more

--- src/day02/day02.ts
+++ src/day02/day02.ts
@@ -1,25 +1,34 @@
 type Color = "red" | "green" | "blue";
 
-const GAME_PATTERN = /^Game (?<index>\d+): (?<subsets>.*)$/;
+const GAME_PATTERN = /^Game (\d+): (.*)$/gm;
+
+function* games(input: string) {
+  for (const [, index, subsets] of input.matchAll(GAME_PATTERN)) {
+    yield { index: Number(index), subsets };
+  }
+}
+
+function* rolls(subsets: string) {
+  for (const subset of subsets.split("; ")) {
+    for (const group of subset.split(", ")) {
+      const [count, rawColor] = group.split(" ");
+      yield { count: Number(count), color: rawColor as Color };
+    }
+  }
+}
 
 export function part1(input: string) {
   let indexSum = 0;
 
-  for (const line of input.trim().split("\n")) {
-    const { index, subsets } = line.match(GAME_PATTERN)!.groups!;
-
+  for (const { index, subsets } of games(input)) {
     const counts: Record<Color, number> = { red: 0, green: 0, blue: 0 };
 
-    for (const subset of subsets.split("; ")) {
-      for (const group of subset.split(", ")) {
-        const [count, rawColor] = group.split(" ");
-        const color = rawColor as Color;
-        counts[color] = Math.max(Number(count), counts[color]);
-      }
+    for (const { color, count } of rolls(subsets)) {
+      counts[color] = Math.max(count, counts[color]);
     }
 
     if (counts.red <= 12 && counts.green <= 13 && counts.blue <= 14) {
-      indexSum += Number(index);
+      indexSum += index;
     }
   }
 
@@ -29,17 +38,11 @@ export function part1(input: string) {
 export function part2(input: string) {
   let powerSum = 0;
 
-  for (const line of input.trim().split("\n")) {
-    const { subsets } = line.match(GAME_PATTERN)!.groups!;
-
+  for (const { subsets } of games(input)) {
     const counts: Record<Color, number> = { red: 0, green: 0, blue: 0 };
 
-    for (const subset of subsets.split("; ")) {
-      for (const group of subset.split(", ")) {
-        const [count, rawColor] = group.split(" ");
-        const color = rawColor as Color;
-        counts[color] = Math.max(Number(count), counts[color]);
-      }
+    for (const { count, color } of rolls(subsets)) {
+      counts[color] = Math.max(count, counts[color]);
     }
 
     powerSum += counts.red * counts.green * counts.blue;
